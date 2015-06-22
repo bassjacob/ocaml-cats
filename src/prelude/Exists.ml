bd3b72e91b718f64baf078cee5081ed1bbce4da2
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 10:21:09 -0600

    Export Def from Exists.Make

--- src/prelude/Exists.ml
+++ src/prelude/Exists.ml
@@ -2,14 +2,18 @@ open Sig
 open Ty.Sig
 
 module Make
-  : functor (E : Unary.Invariant.CO) -> EXISTENTIAL
-      with module T := E
+  : functor (E : Unary.Invariant.CO) ->
+sig
+  module Def : EXISTENTIAL
+    with module T := E
+  include (module type of Def)
+end
   = functor (E : Unary.Invariant.CO) ->
 struct
-  module T = E
-  type 'r elim = { ap : 'x. 'x T.el -> 'r }
   module Def = struct
+    module T = E
     type t = Pack : 'x T.el -> t
+    type 'r elim = { ap : 'x. 'x T.el -> 'r }
     let into x = Pack x
     let from p k = match p with Pack e -> k.ap e
   end
