e256879e806aecc3a044c6a84fa33cef58d79c8b
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-21 20:10:13 -0600

    Formatting

--- src/prelude/Ran.ml
+++ src/prelude/Ran.ml
@@ -9,5 +9,5 @@ struct
   type 'a t = { ran : 'x. ('a -> 'x G.T.el) -> 'x H.T.el }
   type 'f nat = { ap : 'x. ('x G.T.el, 'f) Ty.ap -> 'x H.T.el }
   let into (type f) (module F : Sig.FUNCTOR with type T.tc = f) n f =
-    { ran = fun k -> n.ap (F.T.code %> F.map k %> F.T.elem @@ f) }
+    { ran = fun k -> n.ap %> F.T.code %> F.map k %> F.T.elem @@ f }
 end
