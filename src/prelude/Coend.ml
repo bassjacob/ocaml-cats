0dc11e450c3a337e47296e79a41b88ad73f6a642
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 11:02:40 -0600

    Simplify Coend using Exists

--- src/prelude/Coend.ml
+++ src/prelude/Coend.ml
@@ -1,17 +1,11 @@
 open Sig
 
-(* FIXME: consider using Exists for this *)
 module Make
   : functor (P : PROFUNCTOR) -> COEND
-      with module Hom := P
   = functor (P : PROFUNCTOR) ->
 struct
   module Hom = P
-  module Def = struct
-    type 'r elim = { ap : 'x. ('x, 'x) Hom.T.el -> 'r }
-    type t = Pack : ('x, 'x) Hom.T.el -> t
-    let into x = Pack x
-    let from p k = match p with Pack e -> k.ap e
-  end
+  module Con = Exists.Make(Profunctor.Join(Hom))
+  module Def = Con.Def
   include Def
 end
