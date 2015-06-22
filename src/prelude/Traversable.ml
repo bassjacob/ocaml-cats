3bc6384142a6e6116edc7ae7806b8a263680fbe5
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 14:15:13 -0600

    Just derive sequence

--- src/prelude/Traversable.ml
+++ src/prelude/Traversable.ml
@@ -16,14 +16,7 @@ module List = struct
         | (x::xs) -> (fun h t -> h :: t) <$-> act x <*> go xs in
       A.T.co %> go
 
-    let sequence (type m) (module A : APPLICATIVE with type T.co = m) =
-      let module EFun = Ext.Functor(A) in let open EFun in
-      let module EApp = Ext.Apply  (A) in let open EApp in
-      let act = A.T.el in
-      let rec go xs = match xs with
-        | [] -> A.pure []
-        | (x::xs) -> (fun h t -> h :: t) <$-> act x <*> go xs in
-      A.T.co %> go
+    let sequence m = traverse m Ambient.id
 
   end
   include Def
