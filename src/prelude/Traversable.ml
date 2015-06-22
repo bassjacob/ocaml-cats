c39adf7d769792f544a11979006606460091989f
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 14:12:11 -0600

    Cleanup traverse/sequence a bit

--- src/prelude/Traversable.ml
+++ src/prelude/Traversable.ml
@@ -10,17 +10,19 @@ module List = struct
     let traverse (type m) (module A : APPLICATIVE with type T.co = m) f =
       let module EFun = Ext.Functor(A) in let open EFun in
       let module EApp = Ext.Apply  (A) in let open EApp in
+      let act = A.T.el %> f in
       let rec go xs = match xs with
         | [] -> A.pure []
-        | (x::xs) -> (fun h t -> h :: t) <$-> (A.T.el %> f @@ x) <*> go xs in
+        | (x::xs) -> (fun h t -> h :: t) <$-> act x <*> go xs in
       A.T.co %> go
 
     let sequence (type m) (module A : APPLICATIVE with type T.co = m) =
       let module EFun = Ext.Functor(A) in let open EFun in
       let module EApp = Ext.Apply  (A) in let open EApp in
+      let act = A.T.el in
       let rec go xs = match xs with
         | [] -> A.pure []
-        | (x::xs) -> (fun h t -> h :: t) <$-> (A.T.el x) <*> go xs in
+        | (x::xs) -> (fun h t -> h :: t) <$-> act x <*> go xs in
       A.T.co %> go
 
   end
