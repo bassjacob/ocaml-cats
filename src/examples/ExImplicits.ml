cf0bdad85b87792ffd423c535882aadaac5d2cea
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 21:03:37 -0600

    Update implicits example

--- src/examples/ExImplicits.ml
+++ src/examples/ExImplicits.ml
@@ -3,14 +3,21 @@ open Ambient
 
 (* TODO: port full set of prelude examples to implicits style *)
 
-open Implicits.Semigroup
-open implicit Implicits.Semigroup
+module I = Implicits
+open implicit I.Bifunctor
+open implicit I.Semigroup
 
-let ex0 () : bool =
-  let p0 : bool = op "foo" "bar" = "foobar" in
-  let p1 : bool = (let open implicit Additive in op 3 4) = 7 in
-  let p2 : bool = (let open implicit Multiplicative in op 3 4) = 12 in
+(* Bifunctor for tuple, variant, etc. *)
+let ex1 () : _ =
+  let module M = I.Bifunctor in
+  let _ = M.bimap (fun x -> x * 2) string_of_float (42, 3.14159) in
+  let _ = M.bimap (fun x -> x ^ "bar") (fun y -> y +  1) (Coproduct.inl "foo") in
+  let _ = M.bimap (fun x -> "foo" ^ x) (fun y -> y *. 2.0) (Coproduct.inr 1.0) in
+  ()
+
+let exx () : bool =
+  let module M = I.Semigroup in
+  let p0 : bool = M.op "foo" "bar" = "foobar" in
+  let p1 : bool = (let open implicit M.Additive in M.op 3 4) = 7 in
+  let p2 : bool = (let open implicit M.Multiplicative in M.op 3 4) = 12 in
   p0 && p1 && p2
-
-let () =
-  Printf.printf "%B\n" (ex0 ())
