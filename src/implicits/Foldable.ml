6a30b3a56f4bdfdce2da52ddbb60eeae6c753b5f
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 21:36:26 -0600

    Implicits for foldable

---
+++ src/implicits/Foldable.ml
@@ -0,0 +1,9 @@
+open Prelude.Sig
+
+let foldr (implicit M : FOLDABLE) = M.foldr
+let foldl (implicit M : FOLDABLE) = M.foldl
+let fold_map (implicit M : FOLDABLE) = M.fold_map
+
+implicit module List = Prelude.Foldable.List
+
+implicit module Option = Prelude.Foldable.Option
