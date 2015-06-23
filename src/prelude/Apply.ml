b4c4c0c6e714d8f0617845d9a7e408c39800bb16
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 23:27:27 -0600

    Renaming

--- src/prelude/Apply.ml
+++ src/prelude/Apply.ml
@@ -4,10 +4,10 @@ module List = struct
   module Def = struct
     include Functor.List.Def
     let rec apply : type b. ('a -> b) list -> ('a list -> b list) = fun fs xs ->
-      let module S = Semigroup.List(struct type el = b end) in
+      let module M = Semigroup.List(struct type el = b end) in
       match fs with
       | [] -> []
-      | (f::fs) -> S.op (map f xs) (apply fs xs)
+      | (f::fs) -> M.op (map f xs) (apply fs xs)
   end
   include Def
   include Ext.Apply(Def)
