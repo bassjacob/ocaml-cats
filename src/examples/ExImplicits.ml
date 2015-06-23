f1a3e118bf3fc2cb3aca06f34dae06dfbf712d87
Darin Morrison <darinmorrison+git@gmail.com>
2015-06-22 21:36:44 -0600

    Update implicits example

--- src/examples/ExImplicits.ml
+++ src/examples/ExImplicits.ml
@@ -5,6 +5,7 @@ open Ambient
 
 module I = Implicits
 open implicit I.Bifunctor
+open implicit I.Foldable
 open implicit I.Semigroup
 
 (* Bifunctor for tuple, variant, etc. *)
@@ -13,6 +14,17 @@ let ex1 () : _ =
   let _ = M.bimap (fun x -> x * 2) string_of_float (42, 3.14159) in
   let _ = M.bimap (fun x -> x ^ "bar") (fun y -> y +  1) (Coproduct.inl "foo") in
   let _ = M.bimap (fun x -> "foo" ^ x) (fun y -> y *. 2.0) (Coproduct.inr 1.0) in
+  ()
+
+(* Foldable for option, list, etc. *)
+let ex4 () : _ =
+  let module M = I.Foldable in
+  let module Add = Monoid.Additive.Int in
+  let module Mul = Monoid.Multiplicative.Int in
+  let _ = M.fold_map (module Add) id [1; 2; 3; 4; 5] in
+  let _ = M.fold_map (module Mul) id [1; 2; 3; 4; 5] in
+  let _ = M.fold_map (module Add) id None in
+  let _ = M.fold_map (module Mul) id (Some 5) in
   ()
 
 let exx () : bool =
