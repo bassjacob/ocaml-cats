1e6651198f097f4c48d4bbf6a1bc21b20c14b4fa
Darin Morrison <darinmorrison+git@gmail.com>
2015-08-15 10:24:46 -0600

    Remove unnecessary ;;

--- src/prelude/Ext/Monad.ml
+++ src/prelude/Ext/Monad.ml
@@ -1,8 +1,8 @@
 open Sig
 
 module Make (M : MONAD) = struct
-  module E = Bind.Make(M);;
-  open M;;
-  open E;;
+  module E = Bind.Make(M)
+  open M
+  open E
   let ap mf mx = mf >>= fun f -> mx >>= fun x -> pure (f x)
 end
