0e4904c4d1954c51344a2c0f3597d0ef16d6c724
Darin Morrison <darinmorrison+git@gmail.com>
2015-10-01 10:53:06 -0600

    Formatting

--- src/cats/Ext/Monad.ml
+++ src/cats/Ext/Monad.ml
@@ -1,8 +1,7 @@
 open Sig
 
 module Make (M : MONAD) = struct
-  module E = Bind.Make(M)
-  open M
-  open E
+  module E = Bind.Make(M);; open M;; open E
   let ap mf mx = mf >>= fun f -> mx >>= fun x -> pure (f x)
+  let join mx = bind mx @@ fun x -> x
 end
