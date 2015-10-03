7f0da8f59d58d32fe678c337f2e178b5abb90b9d
Darin Morrison <darinmorrison+git@gmail.com>
2015-10-03 09:59:56 -0600

    Formatting

--- src/cats/Ext/Bifoldable.mli
+++ src/cats/Ext/Bifoldable.mli
@@ -4,7 +4,8 @@ open TyCon
 module Make : functor (M : BIFOLDABLE) -> sig open M
   val bifoldr
      : ('a -> 'c -> 'c)
-    -> ('b -> 'c -> 'c) -> ('c -> ('a, 'b) T.el -> 'c)
+    -> ('b -> 'c -> 'c)
+    -> ('c -> ('a, 'b) T.el -> 'c)
   val bifoldl
      : ('c -> 'a -> 'c)
     -> ('c -> 'b -> 'c)
