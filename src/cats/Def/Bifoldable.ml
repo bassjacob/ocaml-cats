be529f961ac39b4148747ec699110e7de8052c29
Darin Morrison <darinmorrison+git@gmail.com>
2015-10-03 10:03:07 -0600

    Remove unused definitions

--- src/cats/Def/Bifoldable.ml
+++ src/cats/Def/Bifoldable.ml
@@ -5,12 +5,6 @@ module Tuple = struct
   open Amb.Product
   module T = TC.Tuple
 
-  let bifoldr f g i p =
-    f (fst p) (g (snd p) i)
-
-  let bifoldl f g i p =
-    g (f i (fst p)) (snd p)
-
   let bifold_map (type m) (m : m monoid) f g p =
     let module M = (val m) in
     M.op (f (fst p)) (g (snd p))
@@ -19,12 +13,6 @@ end
 module Variant = struct
   open Amb.Coproduct
   module T = TC.Variant
-
-  let bifoldr f g i v = 
-    from (Amb.flip f i) (Amb.flip g i) v
-
-  let bifoldl f g i v =
-    from (f i) (g i) v
 
   let bifold_map _ = from
 end
