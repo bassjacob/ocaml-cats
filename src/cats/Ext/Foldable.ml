bcd1a607ead74d0dcd18c5aee1f0a4fda2eb1b64
Darin Morrison <darinmorrison+git@gmail.com>
2015-10-03 09:49:01 -0600

    Remove unnecessary annotations

--- src/cats/Ext/Foldable.ml
+++ src/cats/Ext/Foldable.ml
@@ -2,13 +2,13 @@ open Sig
 open TyCon
 
 module Make (M : FOLDABLE) = struct open M
-  let rec foldr (type b) (act : _ -> b -> b) init xs =
-    let module B = TC0(struct type t = b end) in
-    let module M = Def.Monoid.Endo(B) in
-    fold_map (module M) act xs @@ init
+  let rec foldr (type m) f init xs =
+    let module M = TC0(struct type t = m end) in
+    let module M = Def.Monoid.Endo(M) in
+    fold_map (module M) f xs @@ init
 
-  let rec foldl (type b) (act : b -> _ -> b) init xs =
-    let module B = TC0(struct type t = b end) in
-    let module M = Def.Monoid.Flip(Def.Monoid.Endo(B)) in
-    fold_map (module M) (Amb.flip act) xs @@ init
+  let rec foldl (type m) f init xs =
+    let module M = TC0(struct type t = m end) in
+    let module M = Def.Monoid.Flip(Def.Monoid.Endo(M)) in
+    fold_map (module M) (Amb.flip f) xs @@ init
 end
